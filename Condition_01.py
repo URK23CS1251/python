e1617c42a693eef9ffa6365c53d6886eb6ae8872
URK23CS1251 <153515089+URK23CS1251@users.noreply.github.com>
2024-02-05 10:15:05 +0530

    Create Condition_01.py

---
+++ Condition_01.py
@@ -0,0 +1,8 @@
+#This code is to find the persons eligibilty to vote
+
+
+x=int(input("enter the age of the person:"))
+if x>=18 :
+  print("yes the person is eligible to vote")
+else :
+  print("No,the person is not eligible to vote")
